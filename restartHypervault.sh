c75f52f89ca1b689385cbfe795b9e05ad2064987
Li Xi <xl404@cam.ac.uk>
2019-01-07 21:32:14 +0000

    Added tearDownDocker functions

--- restartHypervault.sh
+++ restartHypervault.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# first stop and remove all docker containers 
+P1=$(docker ps -q)
+if [ "${P1}" != "" ]; then
+  echo "Killing all running containers"  &2> /dev/null
+  docker kill ${P1}
+fi
+
+P2=$(docker ps -aq)
+if [ "${P2}" != "" ]; then
+  echo "Removing all containers"  &2> /dev/null
+  docker rm ${P2} -f
+fi
+
 ./teardownFabric.sh
 rm -rf ~/.composer
 rm -rf ~/.composer-credentials
