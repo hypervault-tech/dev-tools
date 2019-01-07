21e68c9e1f441b28c89d14f0d8e6d2ff511f439b
Li Xi <xl404@cam.ac.uk>
2019-01-07 21:27:45 +0000

    Removing sudo tag

--- restartHypervault.sh
+++ restartHypervault.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo ./teardownFabric.sh
+./teardownFabric.sh
 rm -rf ~/.composer
 rm -rf ~/.composer-credentials
 rm -rf ~/.composer-connectionProfiles
 
 ./createPeerAdminCard.sh  --host hypervault.tech
 
-sudo ./startFabric.sh
+./startFabric.sh
